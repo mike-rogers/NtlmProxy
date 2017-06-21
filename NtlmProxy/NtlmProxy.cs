f79ec223d0d7e8f8ef60611ce04025b70c56fce1
Chris Mullaley <c_mullaley@hotmail.com>
2017-06-21 14:00:58 +0100

    Expose server so we can test for exceptions.

--- NtlmProxy/NtlmProxy.cs
+++ NtlmProxy/NtlmProxy.cs
@@ -3,150 +3,146 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MikeRogers.NtlmProxy
 {
-    /// <summary>
-    /// Class NtlmProxy. This class cannot be inherited.
-    /// </summary>
-    public sealed class NtlmProxy : IDisposable
-    {
-        #region Fields
+	/// <summary>
+	/// Class NtlmProxy. This class cannot be inherited.
+	/// </summary>
+	public sealed class NtlmProxy : IDisposable
+	{
+		#region Fields
 
-        /// <summary>
-        /// The hostname to which incoming requests will be proxied
-        /// </summary>
-        private readonly Uri _hostname;
+		/// <summary>
+		/// The hostname to which incoming requests will be proxied
+		/// </summary>
+		private readonly Uri _hostname;
 
-        /// <summary>
-        /// The simple HTTP server that accepts proxied requests
-        /// </summary>
-        private readonly SimpleHttpServer _server;
+		/// <summary>
+		/// The simple HTTP server that accepts proxied requests
+		/// </summary>
+		public SimpleHttpServer Server { get; }
 
-        /// <summary>
-        /// The options for the server
-        /// </summary>
-        private readonly SimpleHttpServerOptions _options;
+		/// <summary>
+		/// The options for the server
+		/// </summary>
+		private readonly SimpleHttpServerOptions _options;
 
-        #endregion
-
-
-        #region Properties
-
-        /// <summary>
-        /// Gets the proxy server port.
-        /// </summary>
-        public int Port
-        {
-            get { return _server.Port; }
-        }
-
-        #endregion
+		#endregion
 
 
-        #region Constructors
+		#region Properties
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NtlmProxy"/> class.
-        /// </summary>
-        /// <param name="proxiedHostname">The proxied hostname.</param>
-        /// <param name="serverOptions">Configuration options for the server.</param>
-        public NtlmProxy(Uri proxiedHostname, SimpleHttpServerOptions serverOptions = null)
-        {
-            _options = serverOptions ?? SimpleHttpServerOptions.GetDefaultOptions();
-            _server = new SimpleHttpServer(ProcessRequest, serverOptions);
-            _hostname = proxiedHostname;
-        }
+		/// <summary>
+		/// Gets the proxy server port.
+		/// </summary>
+		public int Port => Server.Port;
 
-        #endregion
+		#endregion
 
 
-        #region Public Methods
+		#region Constructors
 
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-        {
-            _server.Dispose();
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NtlmProxy"/> class.
+		/// </summary>
+		/// <param name="proxiedHostname">The proxied hostname.</param>
+		/// <param name="serverOptions">Configuration options for the server.</param>
+		public NtlmProxy(Uri proxiedHostname, SimpleHttpServerOptions serverOptions = null)
+		{
+			_options = serverOptions ?? SimpleHttpServerOptions.GetDefaultOptions();
+			Server = new SimpleHttpServer(ProcessRequest, serverOptions);
+			_hostname = proxiedHostname;
+		}
 
-        #endregion
+		#endregion
 
 
-        #region Private Methods
+		#region Public Methods
 
-        /// <summary>
-        /// Processes the request.
-        /// </summary>
-        /// <param name="context">The context.</param>
-        /// <returns>A task thread that resolves into an HTTP response.</returns>
-        private async Task<ServerResponse> ProcessRequest(HttpListenerContext context)
-        {
-            var credential = _options.NetworkCredentials ?? CredentialCache.DefaultNetworkCredentials;
-            var myCache = new CredentialCache
-            {
-                {_hostname, "NTLM", credential}
-            };
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			Server.Dispose();
+		}
 
-            var handler = new HttpClientHandler
-            {
-                AllowAutoRedirect = true,
-                Credentials = myCache,
-                UseCookies = true,
-                CookieContainer = new CookieContainer()
-            };
+		#endregion
 
-            handler.CookieContainer.Add(_hostname, context.Request.Cookies);
 
-            using (var client = new HttpClient(handler))
-            {
-                var httpMethod = new HttpMethod(context.Request.HttpMethod);
-                var target = new Uri(_hostname, context.Request.Url.PathAndQuery);
-                var content = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEnd();
+		#region Private Methods
 
-                // New implementation suggested by https://github.com/blabla4
-                var request = new HttpRequestMessage(httpMethod, target);
-                if (content != String.Empty)
-                {
-                    var contentType = context.Request.ContentType;
+		/// <summary>
+		/// Processes the request.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <returns>A task thread that resolves into an HTTP response.</returns>
+		private async Task<ServerResponse> ProcessRequest(HttpListenerContext context)
+		{
+			var credential = _options.NetworkCredentials ?? CredentialCache.DefaultNetworkCredentials;
+			var myCache = new CredentialCache
+			{
+				{_hostname, "NTLM", credential}
+			};
 
-                    if (_options.HasAngularContentType && contentType != null)
-                    {
-                        // Thank you to https://github.com/svantreeck
-                        contentType = Regex.Replace(contentType, ";charset=(.)*", string.Empty);
-                    }
+			var handler = new HttpClientHandler
+			{
+				AllowAutoRedirect = true,
+				Credentials = myCache,
+				UseCookies = true,
+				CookieContainer = new CookieContainer()
+			};
 
-                    request.Content = new StringContent(content, context.Request.ContentEncoding, contentType);
-                }
+			handler.CookieContainer.Add(_hostname, context.Request.Cookies);
 
-                // Add headers ('thank you' to https://github.com/svantreeck)
-                _options.RequestHeaders.ToList().ForEach(x => request.Headers.Add(x.Key, x.Value));
+			using (var client = new HttpClient(handler))
+			{
+				var httpMethod = new HttpMethod(context.Request.HttpMethod);
+				var target = new Uri(_hostname, context.Request.Url.PathAndQuery);
+				var content = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEnd();
 
-                if (_options.AreHeadersDuplicated)
-                {
-                    foreach (var key in context.Request.Headers.AllKeys)
-                    {
-                        if (!_options.ExcludedHeaders.Contains(key))
-                        {
-                            request.Headers.TryAddWithoutValidation(key, context.Request.Headers[key]);
-                        }
-                    }
-                }
+				// New implementation suggested by https://github.com/blabla4
+				var request = new HttpRequestMessage(httpMethod, target);
+				if (content != String.Empty)
+				{
+					var contentType = context.Request.ContentType;
 
-                HttpResponseMessage response = await client.SendAsync(request);
+					if (_options.HasAngularContentType && contentType != null)
+					{
+						// Thank you to https://github.com/svantreeck
+						contentType = Regex.Replace(contentType, ";charset=(.)*", string.Empty);
+					}
 
-                return new ServerResponse
-                {
-                    Message = response,
-                    Cookies = handler.CookieContainer.GetCookies(target)
-                };
-            }
-        }
+					request.Content = new StringContent(content, context.Request.ContentEncoding, contentType);
+				}
 
-        #endregion
-    }
+				// Add headers ('thank you' to https://github.com/svantreeck)
+				_options.RequestHeaders.ToList().ForEach(x => request.Headers.Add(x.Key, x.Value));
+
+				if (_options.AreHeadersDuplicated)
+				{
+					foreach (var key in context.Request.Headers.AllKeys)
+					{
+						if (!_options.ExcludedHeaders.Contains(key))
+						{
+							request.Headers.TryAddWithoutValidation(key, context.Request.Headers[key]);
+						}
+					}
+				}
+
+				HttpResponseMessage response = await client.SendAsync(request);
+
+				return new ServerResponse
+				{
+					Message = response,
+					Cookies = handler.CookieContainer.GetCookies(target)
+				};
+			}
+		}
+
+		#endregion
+	}
 }
